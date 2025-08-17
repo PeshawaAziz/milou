26690ea0de1cfa4151cebf4ea8e87ba0fed7ab3a
Peshawa <modern3p2017@gmail.com>
2025-08-17 16:38:54 +0330

    feat: implement signup service

---
+++ milou/src/main/java/com/milou/service/UserService.java
@@ -0,0 +1,36 @@
+package com.milou.service;
+
+import org.hibernate.SessionFactory;
+
+import com.milou.HibernateSessionFactory;
+import com.milou.model.User;
+
+public class UserService {
+    private final SessionFactory sessionFactory = HibernateSessionFactory.get();
+
+    public User signup(String name, String email, String password) {
+        if (password.length() < 8)
+            throw new IllegalArgumentException("Password must be at least 8 characters.");
+
+        String normalizedEmail = normalizeEmail(email);
+
+        return sessionFactory.fromTransaction(session -> {
+            User existing = session.createQuery("from User where email = :email", User.class)
+                    .setParameter("email", normalizedEmail)
+                    .uniqueResult();
+            if (existing != null)
+                throw new IllegalArgumentException("Email already exists.");
+
+            User user = new User(name, normalizedEmail, password);
+            session.persist(user);
+
+            return user;
+        });
+    }
+
+    private String normalizeEmail(String input) {
+        if (!input.contains("@"))
+            return input + "@milou.com";
+        return input;
+    }
+}
